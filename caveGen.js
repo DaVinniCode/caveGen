ef09bb32550cb26f4ac9d8089862bd0695c5c12b
vince <davinnicode@geemail.fake>
2013-04-26 10:30:47 -0600

    loops in smoothMap added.

--- caveGen.js
+++ caveGen.js
@@ -1,12 +1,14 @@
 // CaveGen test.
 
+
+
 var caveTiles = function(width, height)  {
 	this.cwidth = width;
 	this.cheight = height;
 	this.wallCount = 0;
 	this.floorCount = 0;
 	var tile;
-	this.map = [];
+	this.map = [];	
 	for (var i =0; i <= this.cwidth; i++) {
 		tile = Math.round(Math.random(-1,1));	
 		this.map.push([]);
@@ -22,14 +24,31 @@ var caveTiles = function(width, height)  {
 	}
 
 	this.printMap = function() {
-		for (i in this.map) {console.log(this.map[i].join(""));}
-	}
-}
-	
-	this.smoothMap = function() {
+		for (var i in this.map) {console.log(this.map[i].join(""));}
+	};
+
+	this.smoothMap = function(times) {
 		// First get a list of tiles to change THEN perform the change after all changes are determined.
-		
-	}
+		this.changeList = [];
+		var floor = 0; var wall = 0;
+		for (var row in this.map) {			
+			for (var column in this.map[row]) { 
+				for (var x = -1; x < 2; x ++) {
+					for (var y = -1; y < 2; y++) {
+						console.log("x: "+x+" , y: "+y);
+					}
+				}                     
+				if (this.map[row][column] === '#') {wall++;}               // If row +/- 1 out of bounds ( 0<x<width), don't try to check it
+				// console.log("row: "+row+ " column: "+column);	      // If column +/- 1 out of bounds ( 0 < y < height), don't try to check it.
+			}
+		}
+
+	};
+
+
+};
+	
+
 
 
 
